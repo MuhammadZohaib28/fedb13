a1d30ed3f254b6af0ea3e367f73018eeeed77fd8
Muhammad Zohaib <zohaibsoomro100@gmail.com>
2025-12-29 22:06:53 +0500

    variables in JS

---
+++ day1.js
@@ -0,0 +1,17 @@
+console.log("Zohaib")
+
+//DATA TYPES 
+// STRING
+// NUMBER
+// BOOLEAN  
+// ARRAY
+// OBJECT
+// BIG INT
+
+
+console.log(10 + 20)
+
+console.log("60" * "20")
+
+console.log("10" + "20")
+
---
+++ day2.js
@@ -0,0 +1,75 @@
+console.log("Zohaib")
+
+// VARIABLES IN JAVASCRIPT
+// var
+// const
+// let
+
+
+var firstName = "Dania";
+
+var lastName = "Farhan"
+
+console.log("Kese hain app")
+
+
+console.log(firstName + " " + lastName)
+
+
+console.log("Muzammil" + " " + "Ijaz")
+
+
+
+var userAge = 24;
+
+console.log("User name is " + firstName + " " + lastName + " and user age is " + userAge)
+
+
+var firstName = "Hamza"
+
+console.log(firstName)
+
+
+
+
+var userAge = 25;
+userAge = 26;
+
+
+console.log(userAge, "Yeh mene isko update kiya hai")
+
+
+
+// ES6 MODULE
+
+
+var newAge = 23;
+newAge = 25;
+
+console.log(newAge)
+
+
+
+
+let newAge1 = "28";
+
+console.log(newAge1)
+
+
+let year = new Date().getDay();
+
+console.log(year)
+
+
+let isRaining = false;
+
+if (isRaining) {
+    console.log("Ajj Class Nahi Hogi")
+} else {
+    console.log("Ajj Class Hogi")
+}
+
+const database_key = 1232323234234;
+
+
+console.log(database_key)
